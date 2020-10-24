2d9f9649590e872a8f2e9cf72b57c891882e9a5c
timur <buharaman@gmail.com>
2020-10-24 22:50:27 +0300

    it's alive!

---
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -0,0 +1,22 @@
+package web.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarsController {
+    @GetMapping(value = "/cars")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Cars!");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+
+}
